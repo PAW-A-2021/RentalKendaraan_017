a5c059e3966910dbd0f811a6907f47c8d37a86cb
yahyaluqman <yahya.luqmanu21@gmail.com>
2021-11-27 12:42:11 +0700

    Update

--- RentalKendaraan/Controllers/HomeController.cs
+++ RentalKendaraan/Controllers/HomeController.cs
@@ -23,15 +23,6 @@ namespace RentalKendaraan.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
